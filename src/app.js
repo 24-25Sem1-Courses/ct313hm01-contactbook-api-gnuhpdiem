e52017237b8cca16dbb1d8c0a9c865d7e4b7921c
Diem Phung <phungb2112000@student.ctu.edu.vn>
2024-09-17 17:27:36 +0700

    define routes and controller for contacts

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const cors = require('cors')
+
+const contactsRouter = require('./routes/contacts.router')
 const app = express()
 
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
-    res.send('Hello')
+    return res.json({message: 'ok'})
 })
+
+contactsRouter.setup(app)
 
 module.exports = app
