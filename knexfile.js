e62acd7ebe850a8f845d9a22dc90af891cb90159
Nguyen Duy Diem Phung <phungb2112000@student.ctu.edu.vn>
2024-09-18 08:54:57 +0700

    Setup knex.js, mysql and insert fake data using faker.js

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+// Update with your config settings.
+require('dotenv').config()
+
+const {DB_HOST, DB_PORT, DB_PASS, DB_USER, DB_NAME} = process.env
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  client: 'mysql',
+  connection: {
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASS,
+    database: DB_NAME,
+  },
+  pool: {
+    min: 0,
+    max:10
+  },
+  seeds: {
+    directory: './seeds',
+  },
+
+};
